80529e568204ee5310807e3db6e5e5e0f605ab9d
nittothomas <nittothomas94@example.com>
2025-07-09 13:05:14 +0530

    connected to mongodb atlas

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose');
 //mongoose driver for connecting to databse
 
+require('dotenv').config();
+
 //cooncetion path(conncetion string) to the DataBase
 mongoose
-  .connect('mongodb://localhost:27017/e-commerce')
+  .connect(process.env.MONGODB_URI)
+  // mongodb://localhost:27017/e-commerce
+
   .then(() => console.log('DB Connected'))
   .catch(e => console.log(e));
 
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 
+require('dotenv').config();
+
 const app = express();
-const port = 8000;
+const port = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
